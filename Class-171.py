25b642bfff8fdf3eb68afa4f850b239f98f0f991
HorizonSRE <106289299+HorizonSRE@users.noreply.github.com>
2024-01-27 17:46:07 -0800

    Add files via upload

---
+++ Class-171.py
@@ -0,0 +1,72 @@
+from tkinter import*
+from PIL import ImageTk, Image
+from datetime import datetime
+import pytz
+import time
+
+root=Tk()
+root.geometry("1000x850")
+clock_image = ImageTk.PhotoImage(Image.open("clockimg.jpeg"))
+
+
+india_label = Label(root, text="India")
+india_label.place(relx=0.2, rely=0.05, anchor=CENTER)
+
+india_clock = Label(root)
+india_clock["image"] = clock_image
+india_clock.place(relx=0.2, rely=0.35, anchor=CENTER)
+india_time = Label(root)
+india_time.place(relx=0.2, rely=0.65, anchor=CENTER)
+
+
+
+
+usa_label = Label(root, text="United States of America")
+usa_label.place(relx=0.7, rely=0.05, anchor=CENTER)
+
+usa_clock = Label(root)
+usa_clock["image"] = clock_image
+usa_clock.place(relx=0.7, rely=0.35, anchor=CENTER)
+
+usa_time = Label(root)
+usa_time.place(relx=0.7, rely=0.65, anchor=CENTER)
+
+
+
+
+aus_label = Label(root, text="Australia")
+aus_label.place(relx=0.2, rely=0.05, anchor=CENTER)
+
+aus_clock = Label(root)
+aus_clock["image"] = clock_image
+aus_clock.place(relx=0.2, rely=0.35, anchor=CENTER)
+aus_time = Label(root)
+aus_time.place(relx=0.2, rely=0.65, anchor=CENTER)
+
+
+
+
+jpn
+class India():
+    def time(self):
+        home=pytz.timezone("Asia/Kolkata")
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%H:%M:%S")
+        india_time["text"]="Time: "+current_time
+        india_time.after(200, self.time)
+
+class USA():
+    def time(self):
+        home=pytz.timezone("US/Central")
+        local_time=datetime.now(home)
+        current_time=local_time.strftime("%H:%M:%S")
+        usa_time["text"]="Time: "+current_time
+        usa_time.after(200, self.time)
+        
+obj_india=India()
+obj_usa=USA()
+india_btn = Button(root, text="Show the Time in Kolkata,India", command=obj_india.time())
+india_btn.place(relx=0.2, rely=0.8, anchor=CENTER)
+usa_btn = Button(root, text="Show the Time in Central USA", command=obj_usa.time())
+usa_btn.place(relx=0.7, rely=0.8, anchor=CENTER)
+root.mainloop()
